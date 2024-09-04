0cf2cb14fe0162d4fdf2655635a5a4ce6ee5c4fe
Sujan <sujanvaland@gmail.com>
2024-09-04 14:58:05 +0530

    fix circular dependency

--- src/main/java/com/springrest/springrest/entities/ContactDetail.java
+++ src/main/java/com/springrest/springrest/entities/ContactDetail.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactDetail {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
